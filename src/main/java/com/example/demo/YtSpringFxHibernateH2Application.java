a91e5d375aa460b9407b9ba5cd8b665eb58e6c72
Bartek <bartlomiejmont@gmail.com>
2020-05-06 00:49:44 +0200

    inital commit

---
+++ src/main/java/com/example/demo/YtSpringFxHibernateH2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtSpringFxHibernateH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtSpringFxHibernateH2Application.class, args);
+	}
+
+}
